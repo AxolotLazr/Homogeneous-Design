0331cb971bde3383c681beb12c990de670baff0f
AxolotLazr <105299618+AxolotLazr@users.noreply.github.com>
2024-08-19 23:19:42 -0700

    BIG ASS COMMIT (mb)

--- scripts/shared-scripts.js
+++ scripts/shared-scripts.js
@@ -1,14 +1,25 @@
-let ColorTheme = 'light';
+let ColorTheme = false;
+let HotTheme1 = 'corpo-dark';
+let HotTheme2 = 'plum';
+let SetTheme = '';
 
-switchTheme();
+updateTheme();
 
-function switchTheme() {
-    if (document.getElementsByClassName('the-bottom-theme-switch')[0].getElementsByTagName('input')[0].checked) {
-        ColorTheme = 'dark';
+function toggleTheme() {
+    ColorTheme = !ColorTheme;
+
+    updateTheme();
+}
+function updateTheme() {
+    if (ColorTheme) {
+        SetTheme = HotTheme2;
+        document.getElementsByClassName('the-bottom-theme-icon')[0].setAttribute('src', 'images/ours/uncolored/LightIcon.png');
     } else {
-        ColorTheme = 'light';
+        SetTheme = HotTheme1;
+        document.getElementsByClassName('the-bottom-theme-icon')[0].setAttribute('src', 'images/ours/uncolored/DarkIcon.png');
     }
-    document.getElementById('color-theme').setAttribute('href', 'styles/ours/'+ColorTheme+'-theme.css')
+    
+    document.getElementById('color-theme').setAttribute('href', 'styles/ours/'+SetTheme+'-theme.css');
 }
 
 function checkTheme() {
