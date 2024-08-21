d39a80ae775232565ff5907df77b0f82ada86914
AxolotLazr <105299618+AxolotLazr@users.noreply.github.com>
2024-08-20 20:33:22 -0700

    8/20/2024 a good days work

--- scripts/homogenize-script.js
+++ scripts/homogenize-script.js
@@ -1,11 +1,15 @@
+let homoName = document.getElementsByClassName('homogenized-script')[0].getAttribute('name')
+
 function updateTheme() {
     if (ColorTheme) {
         SetTheme = HotTheme2;
         document.getElementsByClassName('the-bottom-theme-icon')[0].setAttribute('src', '../images/ours/uncolored/LightIcon.png');
+        document.getElementById('homogenized-color-theme').setAttribute('href', '../styles/theirs/'+homoName+'-light-theme.css');
     } else {
         SetTheme = HotTheme1;
         document.getElementsByClassName('the-bottom-theme-icon')[0].setAttribute('src', '../images/ours/uncolored/DarkIcon.png');
+        document.getElementById('homogenized-color-theme').setAttribute('href', '../styles/theirs/'+homoName+'-dark-theme.css');
     }
-    
+       
     document.getElementById('color-theme').setAttribute('href', '../styles/ours/'+SetTheme+'-theme.css');
 }
