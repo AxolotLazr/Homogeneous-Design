9f5cbeb048f380f931085e9b9b4afbb3994c2156
AxolotLazr <105299618+AxolotLazr@users.noreply.github.com>
2024-08-26 20:31:10 -0700

    Commit from school (done late)

--- scripts/shared-script.js
+++ scripts/shared-script.js
@@ -5,6 +5,18 @@ let SetTheme = '';
 
 updateTheme();
 
+function updateTheme() {
+    if (ColorTheme) {
+        SetTheme = HotTheme2;
+        document.getElementsByClassName('the-bottom-theme-icon')[0].setAttribute('src', 'images/ours/uncolored/LightIcon.png');
+    } else {
+        SetTheme = HotTheme1;
+        document.getElementsByClassName('the-bottom-theme-icon')[0].setAttribute('src', 'images/ours/uncolored/DarkIcon.png');
+    }
+    
+    document.getElementById('color-theme').setAttribute('href', 'styles/ours/'+SetTheme+'-theme.css');
+}
+
 function toggleTheme() {
     ColorTheme = !ColorTheme;
 
