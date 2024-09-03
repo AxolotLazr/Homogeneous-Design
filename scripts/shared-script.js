b44b32dcd7fdd8914edd704cb86b8be95b089e33
AxolotLazr <105299618+AxolotLazr@users.noreply.github.com>
2024-09-03 14:55:22 -0700

    End of school day 9/3/2024

--- scripts/shared-script.js
+++ scripts/shared-script.js
@@ -8,10 +8,10 @@ updateTheme();
 function updateTheme() {
     if (ColorTheme) {
         SetTheme = HotTheme2;
-        document.getElementsByClassName('the-bottom-theme-icon')[0].setAttribute('src', 'images/ours/uncolored/LightIcon.png');
+        document.getElementById('the-theme-icon').style.maskImage = "url(SVGs/ours/themeLight.svg), url(SVGs/ours/search.svg)";
     } else {
         SetTheme = HotTheme1;
-        document.getElementsByClassName('the-bottom-theme-icon')[0].setAttribute('src', 'images/ours/uncolored/DarkIcon.png');
+        document.getElementById('the-theme-icon').style.maskImage = "url(SVGs/ours/themeDark.svg), url(SVGs/ours/search.svg)";
     }
     
     document.getElementById('color-theme').setAttribute('href', 'styles/ours/'+SetTheme+'-theme.css');
