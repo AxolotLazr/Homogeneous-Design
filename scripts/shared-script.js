ced267d0fc0635ee82cd31174ae25047420b48ee
AxolotLazr <105299618+AxolotLazr@users.noreply.github.com>
2024-09-03 23:30:13 -0700

    A good days work

--- scripts/shared-script.js
+++ scripts/shared-script.js
@@ -8,10 +8,10 @@ updateTheme();
 function updateTheme() {
     if (ColorTheme) {
         SetTheme = HotTheme2;
-        document.getElementById('the-theme-icon').style.maskImage = "url(SVGs/ours/themeLight.svg), url(SVGs/ours/search.svg)";
+        document.getElementById('the-theme-icon').style.maskImage = "url(SVGs/ours/themeLight.svg)";
     } else {
         SetTheme = HotTheme1;
-        document.getElementById('the-theme-icon').style.maskImage = "url(SVGs/ours/themeDark.svg), url(SVGs/ours/search.svg)";
+        document.getElementById('the-theme-icon').style.maskImage = "url(SVGs/ours/themeDark.svg)";
     }
     
     document.getElementById('color-theme').setAttribute('href', 'styles/ours/'+SetTheme+'-theme.css');
