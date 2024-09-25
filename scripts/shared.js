25f7307b0854202f4970513ab80df9323257ea32
AxolotLazr <105299618+AxolotLazr@users.noreply.github.com>
2024-09-24 17:03:37 -0700

    fix links OH GOD LET THIS BE OVER

--- scripts/shared.js
+++ scripts/shared.js
@@ -135,7 +135,7 @@ for (i = 0; i < pages.length; i++){
     let newCard = document.createElement('a');
     newCard.id = pages[i].name;
     newCard.classList = 'card outline glow';
-    newCard.href = pages[i].link + '/';
+    newCard.href = (window.location.hostname.includes('io') ? '/Homogenous-Design':'')+pages[i].link+'/';
     if (pages[i].link == currentPage){newCard.style.opacity = '0.5';}
         let newCardTitle = document.createElement('div');
         newCardTitle.classList = 'title';
@@ -144,7 +144,7 @@ for (i = 0; i < pages.length; i++){
         
         let newCardCreator = document.createElement('a');
         newCardCreator.classList = 'creator outline glow';
-        newCardCreator.href = pages[i].creator.page.link;
+        newCardCreator.href = (window.location.hostname.includes('io') ? '/Homogenous-Design':'')+pages[i].creator.page.link;
         newCardCreator.style.backgroundImage = 'url('+hrefPrefix+'images/PFPs/'+pages[i].creator.pfp.image+')';
         newCardCreator.style.backgroundSize = 'calc(100%*'+pages[i].creator.pfp.zoom+')';
         newCardCreator.style.backgroundPosition = pages[i].creator.pfp.offsetX*100+'% '+pages[i].creator.pfp.offsetY*100+'%';
